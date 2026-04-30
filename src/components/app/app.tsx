e67a7dcf44f2655c4eaa59bf5961149808cf0c0c
Stepan Vasilyev <intellectualspecialist@gmail.com>
2026-04-30 20:18:52 +0500

    feat: добавлены компаненты страниц (экранов) приложения и компонент App

---
+++ src/components/app/app.tsx
@@ -0,0 +1,17 @@
+import BookingPage from '../../pages/booking-page/booking-page';
+import ContactsPage from '../../pages/contacts-page/contacts-page';
+import LoginPage from '../../pages/login-page/login-page';
+import MainPage from '../../pages/main-page/main-page';
+import MyQuestsPage from '../../pages/my-quests-page/my-quests-page';
+import QuestPage from '../../pages/quest-page/quest-page';
+
+const App = (): JSX.Element => (
+  <MainPage />
+  // <LoginPage />
+  // <BookingPage />
+  // <ContactsPage />
+  // <MyQuestsPage />
+  // <QuestPage />
+);
+
+export default App;
